043b85cc72fda21c3608efb93b373af71f1f238a
Pankaj Kumar Maurya <153347011+pankajm1995@users.noreply.github.com>
2024-04-03 06:15:03 +0000

    update

--- demo/src/Component/CardsDetails.js
+++ demo/src/Component/CardsDetails.js
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import Table from 'react-bootstrap/Table';
 const CardsDetails = () => {
   return (
     <div>
-        <h1>Card 2</h1>
+        <>
+          <div className='container mt-2 '>
+            <h2 className='text-text-center'>Item Details</h2>
+              <section className='container mt-3 border border-danger'>
+                <div className='item detail d-flex'>
+                  <div className='item-img'>
+                    <img src="https://cdn.pixabay.com/photo/2022/06/07/20/52/curry-7249247_640.jpg" alt="dish"/>
+                  </div>
+                  <div className='details'>
+                    <Table>
+                      <tr>
+                        <td>
+                          <p><strong>Restoraunt</strong>:Keyur Dhaba</p>
+                          <p><strong>Price</strong>:₹ 450</p>
+                          <p><strong>Dishes</strong>: Western India</p>
+                          <p><strong>Total</strong>: ₹ 450</p>
+                          <p><stron>Remove</stron><span><i class="fa fa-trash color" aria-hidden="true" style={{cursor:'pointer'}}></i></span></p>
+                        </td>
+                        <td>
+                          <p><strong>Rating</strong>:<span style={{margin:'10px',padding:'5px'}}>De do jo dena hai</span></p>
+                          <p><strong>Order Review</strong>: ⭐ ⭐ ⭐ ⭐ ⭐ </p>
+                        </td>
+                      </tr>
+                    </Table>
+                  </div>
+                </div>
+              </section>
+          </div>
+        </>
     </div>
   )
 }
